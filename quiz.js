2ecd164be471c603d2512847dbe8116b13e61add
Addit <adam.garza33@hotmail.com>
2023-02-22 02:34:12 -0600

    defined my variables w const

--- quiz.js
+++ quiz.js
@@ -1,15 +1,39 @@
 const startBtn = document.getElementById('start-btn')
+const questionContainerEl = document.getElementById('question-container')
+const questionsEl = document.getElementById('question')
+const answerButtonEl = document.getElementById('answer-buttons')
+
+let randomQuestions, currentQuestionIndex
 
 startBtn.addEventListener('click', startQuiz)
 
 function startQuiz() {
   console.log('Started')
+  startBtn.classList.add('hide')
+  randomQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerEl.classList.remove('hide')
+  setNextQuestion()
 }
 
 function setNextQuestion() {
+  revealQuestion(randomQuestions[currentQuestionIndex])
+}
 
+function revealQuestion(question) {
+  questionsEl.innerText = question.question
 }
 
 function selectAnswer() {
 
 }
+
+const questions = [
+    {
+        question: 'What does CSS stand for?',
+        answer: [
+            {text: 'Cascading Style Sheets', correct: true},
+            {text: 'Corrupted Star Shell', correct: false}
+        ]
+    }
+]
