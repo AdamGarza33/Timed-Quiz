09f4e7d44a90f3ffb8f54340c1a00f5660812cf3
Addit <adam.garza33@hotmail.com>
2023-03-16 23:48:33 -0500

    added functionallity to JavaScript code to ask more questions and answering right continues to the next question

--- quiz.js
+++ quiz.js
@@ -1,51 +1,125 @@
-const startButton = document.getElementById('start-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
+const StrtBtn = document.getElementById('start-btn')
+const nxtBtn = document.getElementById('next-btn')
+const questionsContainerEl = document.getElementById('question-container')
+const questionEl = document.getElementById('question')
+const answerBtnEl = document.getElementById('answer-buttons')
 
 let randomQuestions, currentQuestionIndex
 
-startButton.addEventListener('click', startGame)
+StrtBtn.addEventListener('click', startQuiz)
+nxtBtn.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
 
-function startGame() {
-    console.log('Started')
-    startButton.classList.add('hide')
-    randomQuestions = questions.sort(()=> Math.random() - .5)
-    currentQuestionIndex = 0
-    questionContainerElement.classList.remove('hide')
-    setNextQuestion()
+function startQuiz() {
+  StrtBtn.classList.add('hide')
+  randomQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionsContainerEl.classList.remove('hide')
+  setNextQuestion()
 }
 
 function setNextQuestion() {
-  showQuestion(randomQuestions[currentQuestionIndex])
+  resetQuiz()
+  revealQuestion(randomQuestions[currentQuestionIndex])
 }
 
-function showQuestion(question) {
-    questionElement.innerText = question.question
-    question.answers.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
-        
+function revealQuestion(question) {
+  questionEl.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerBtnEl.appendChild(button)
+  })
 }
 
+function resetQuiz() {
+  clearStatusClass(document.body)
+  nxtBtn.classList.add('hide')
+  while (answerBtnEl.firstChild) {
+    answerBtnEl.removeChild(answerBtnEl.firstChild)
+  }
+}
 
-function selectAnswer() {
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerBtnEl.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (randomQuestions.length > currentQuestionIndex + 1) {
+    nxtBtn.classList.remove('hide')
+  } else {
+    StrtBtn.innerText = 'Restart'
+    StrtBtn.classList.remove('hide')
+  }
+}
 
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
 }
 
 const questions = [
-    {
-        question: 'What does CSS stand for?',
-        answers: [
-            { text: 'Cascading Style Sheets', correct: true },
-            { text: 'Cool Stuff Star', correct: false }
-        ]
-    }
+  {
+    question: 'Inside which HTML element do we put the JavaScript?',
+    answers: [
+      { text: '<script>', correct: true },
+      { text: '<js>', correct: false }
+    ]
+  },
+  {
+    question: 'Which operator is used to assign a value to a variable?',
+    answers: [
+      { text: 'ver', correct: false },
+      { text: 'var', correct: true },
+      { text: '*', correct: false },
+      { text: '-', correct: false }
+    ]
+  },
+  {
+    question: 'How do you create a function in JavaScript?',
+    answers: [
+      { text: 'function myFunction()', correct: true },
+      { text: 'function = myFunction()', correct: false },
+      { text: 'function:myFunction()', correct: false }
+    ]
+  },
+  {
+    question: 'What is the correct way to write a JavaScript array',
+    answers: [
+      { text: 'var colors = "red", "green", "blue"', correct: false },
+      { text: 'var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")', correct: false },
+      { text: 'var colors = ["red", "green", "blue"] ', correct: true}
+    ]
+  }
 ]
+
+
+var count = 15;
+var interval = setInterval(function(){
+  document.getElementById('count').innerHTML=count;
+  count--;
+  if (count === 0){
+    clearInterval(interval);
+    document.getElementById('count').innerHTML='Finished';
+    // or...
+    alert("You're out of time! Better luck next time :/");
+  }
+}, 1000);
